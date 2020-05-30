1ece83c339d6aed0c3d36dc84df6a7f01f7b8cf0
PaintDream <paintdream@paintdream.com>
2020-05-31 01:35:01 +0800

    Add missing namespace.

--- Template/TTagged.h
+++ Template/TTagged.h
@@ -4,45 +4,47 @@
 //
 
 #ifndef __TTAGGED_H__
-#define __TTAGEED_H__
+#define __TTAGGED_H__
 
 #include "../Interface/IType.h"
 
-template <class T, size_t N>
-class TTagged {
-public:
-	typedef T Type;
-	enum { MASK = (1 << N) - 1 };
+namespace PaintsNow {
+	template <class T, size_t N>
+	class TTagged {
+	public:
+		typedef T Type;
+		enum { MASK = (1 << N) - 1 };
 
-	TTagged(T ptr = nullptr, size_t tag = 0) : storage(reinterpret_cast<T>((size_t)ptr | tag)) {
-		assert(((size_t)ptr & MASK) == 0);
-		assert(tag <= MASK);
-	}
+		TTagged(T ptr = nullptr, size_t tag = 0) : storage(reinterpret_cast<T>((size_t)ptr | tag)) {
+			assert(((size_t)ptr & MASK) == 0);
+			assert(tag <= MASK);
+		}
 
-	size_t Tag() const {
-		return (size_t)storage & MASK;
-	}
+		size_t Tag() const {
+			return (size_t)storage & MASK;
+		}
 
-	void Tag(size_t tag) {
-		assert(tag <= MASK);
-		storage = reinterpret_cast<T>(((size_t)storage & ~MASK) | tag);
-	}
+		void Tag(size_t tag) {
+			assert(tag <= MASK);
+			storage = reinterpret_cast<T>(((size_t)storage & ~MASK) | tag);
+		}
 
-	void operator () (T t) {
-		assert(((size_t)t & MASK) == 0);
-		storage = reinterpret_cast<T>((size_t)t | ((size_t)storage & MASK));
-	}
+		void operator () (T t) {
+			assert(((size_t)t & MASK) == 0);
+			storage = reinterpret_cast<T>((size_t)t | ((size_t)storage & MASK));
+		}
 
-	const T operator () () const {
-		return reinterpret_cast<T>((size_t)storage & ~MASK);
-	}
+		const T operator () () const {
+			return reinterpret_cast<T>((size_t)storage & ~MASK);
+		}
 
-	T operator () () {
-		return reinterpret_cast<T>((size_t)storage & ~MASK);
-	}
+		T operator () () {
+			return reinterpret_cast<T>((size_t)storage & ~MASK);
+		}
 
-private:
-	T storage;
-};
+	private:
+		T storage;
+	};
+}
 
 #endif // __TTAGGED_H__
